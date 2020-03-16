269e80a6127d33df346843546a41cdd54c163799
nola360 <adaramolajr@gmail.com>
2020-03-15 23:06:18 -0400

    changed div i tag and added inline style color grn

--- app.js
+++ app.js
@@ -7,8 +7,52 @@ function animatedForm() {
       const parent = arrow.parentElement;
       const nextForm = parent.nextElementSibling;
 
+      //Check for validation
+      if (input.type === 'text' && validateUser(input)) {
+        nextSlide(parent, nextForm);
+      } else if (input.type === 'email' && validateemail(input)) {
+        nextSlide(parent, nextForm);
+      } else if (input.type === 'password' && validateUser(input)) {
+        nextSlide(parent, nextForm);
+      }
     })
   })
 }
 
 animatedForm();
+
+
+//this function takes in a user input
+function validateUser(user) {
+  if (user.value.length < 6) {
+    console.log('Not enough info');
+    error('rgb(189,87,87)');
+  } else {
+    error('rgb 87. 189, 130');
+    return true;
+  }
+}
+
+function validateemail(email) {
+  const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; /*regex*/
+  if (validation.test(email.value)) {
+    error('rgb 87. 189, 130');
+    return true;
+  } else {
+    error('rgb(189,87,87)');
+  }
+
+}
+
+
+function nextSlide(parent, nextForm) {
+  parent.classList.add('inactive');
+  parent.classList.remove('active');
+  nextForm.classList.add('active'); /* Next form transitions in*/
+}
+
+function error(color) {
+  document.body.style.backgroundColor = color;
+}
+
+
